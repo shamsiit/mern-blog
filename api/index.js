56ff328a1bbadfdab592423ae813101dc78e353f
shahriar <shahriar@moleculedata.com>
2024-05-12 18:00:44 +0600

    Add auth and user route and cintroller

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -18,3 +20,6 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000 !!');
 }
 );
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
