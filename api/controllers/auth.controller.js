f966dfb66a74525b6affb700b9ea363c419831ca
shahriar <shahriar@moleculedata.com>
2024-05-28 20:54:21 +0600

    Added signup API

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,30 @@
-export const signup = async (req, res) => {
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-    console.log(req.body);
+export const signup = async (req, res, next) => {
+
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password || username === '' || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required!'));
+    }
+
+    const hashedPasword = bcryptjs.hashSync(password, 10);
+
+    const newUSer = new User({
+        username,
+        email,
+        password: hashedPasword
+
+    });
+
+    try {
+        await newUSer.save();
+        res.json('Signup successful');
+    } catch (error) {
+        next(error);
+    }
+
+   
 };
--- api/index.js
+++ api/index.js
@@ -15,6 +15,7 @@ mongoose.connect(
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000 !!');
@@ -23,3 +24,13 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error!';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
